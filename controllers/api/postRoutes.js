4a1f717ee2168867f83cf5a1d8401471f9913f1b
David Galvez <david.dmg17@gmail.com>
2022-08-20 09:56:46 -0700

    created the post and create user

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth')
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = Post.create({
+            userId: req.session.userId,
+            title: req.body.title,
+            body: req.body.body
+        });
+        res.json(newPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+});
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = User.create({
+            userName: req.body.userName,
+            password: req.body.password
+        });
+        res.json(newUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
