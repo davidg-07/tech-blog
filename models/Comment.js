aff060a9b314913d238c060a9f495b4c76ebbac3
David Galvez <david.dmg17@gmail.com>
2022-09-04 20:11:17 -0700

    added a helper

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 class Comment extends Model { }
 
 Comment.init({
-    body: {type: DataTypes.STRING, allowNull: false}
+    body: { type: DataTypes.STRING, allowNull: false }
 }, { sequelize });
 
 module.exports = Comment;
